52b7ead1e0880ed7eceff46cf9cfd19409624505
tgfukuda <luktiger793@gmail.com>
2022-09-01 16:23:45 +0900

    first spell on goerli

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -20,6 +20,7 @@ pragma solidity 0.6.12;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
+import "dss-interfaces/Interfaces.sol";
 
 // import { DssSpellCollateralAction } from "./DssSpellCollateral.sol";
 
@@ -32,11 +33,7 @@ contract DssSpellAction is DssAction {
     // This should be modified weekly to provide a summary of the actions
     // Hash: cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/f00be45def634c9185f4561a8f1549f28e704d11/governance/votes/Executive%20vote%20-%20August%2024%2C%202022.md -q -O - 2>/dev/null)"
     string public constant override description =
-        "2022-08-24 MakerDAO Executive Spell | Hash: 0x47527eb9c08846cf35e779272c00c60f86ce6a5a745b46772ba5747d22e893e2";
-
-    uint256 public constant WAD = 10**18;
-
-    uint256 public constant RWA009_DRAW_AMOUNT = 25_000_000 * WAD;
+        "2022-09-01 Fix PIP_ETH, PIP_FAU src";
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -58,8 +55,16 @@ contract DssSpellAction is DssAction {
         // ----------------------------- RWA Draws -----------------------------
         // https://vote.makerdao.com/polling/QmQMDasC#poll-detail
         // Weekly Draw for HVB
-        address RWA009_A_URN = DssExecLib.getChangelogAddress("RWA009_A_URN");
-        RwaUrnLike(RWA009_A_URN).draw(RWA009_DRAW_AMOUNT);
+        
+        address ethMedian = 0xad515b7a09a4A4A2AD6A87a98318A67aaaB5D3Ce;
+        address fauMedian = 0x5210B794BA8A0c90a6DC1bFA6c9C00037C23e0b4;
+
+        address pipEth = ChainlogAbstract(DssExecLib.LOG).getAddress("PIP_ETH");
+        address pipFau = ChainlogAbstract(DssExecLib.LOG).getAddress("PIP_FAU");
+
+
+        OsmAbstract(pipEth).change(ethMedian);
+        OsmAbstract(pipFau).change(fauMedian);
     }
 }
 
